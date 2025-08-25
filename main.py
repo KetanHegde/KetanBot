9f5b7b96c48ed0f00d7d1cdd5f4bf2c8fae644cf
KetanHegde <ketanhegde2003@gmail.com>
2025-08-25 21:06:15 +0530

    Add github CORS

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app = FastAPI()
 origins = [
     "http://127.0.0.1:5500",  
     "http://localhost:5500",
-    "https://ketanhegde.github.io/" 
+    "https://ketanhegde.github.io" 
 ]
 
 app.add_middleware(
